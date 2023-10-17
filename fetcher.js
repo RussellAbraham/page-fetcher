7e9a15bdd44a4309c22c8eb1ecacd0ee80bcc34a
RussellAbraham <russell_a@live.com>
2023-10-16 20:25:44 -0700

    Add helper function to writeFile and add readline

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,47 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
-// get the arguments from the command line
-const args = process.argv.slice(2);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const fetchPage = (url, file) => {
   try {
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        fs.writeFile(file, body, (err) => {
-          if (err) {
-            console.error(err);
-          }
-        });
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching the page:', error);
+      } else if (response.statusCode === 200) {
+        console.log(body, file);
+      } else {
+        console.error('Unexpected status code:', response.statusCode);
       }
     });
-  } catch(er) {
-    console.error(er.message);
-  } 
+  } catch (err) {
+    console.error('An unexpected error occurred:', err.message);
+  }
 };
 
-fetchPage(args[0], args[1]);
+const writeFile = (body, file) => {
+  fs.writeFile(file, body, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Page fetched and saved successfully!');
+    }
+    rl.close();
+  });
+};
+
+const args = process.argv.slice(2);
+
+if (args.length !== 2) {
+  console.error('Usage: node script.js <url> <output-file>');
+  rl.close();
+} else {
+  fetchPage(args[0], args[1]);
+  rl.close();
+}
+
+rl.on('close', () => process.exit(0));
